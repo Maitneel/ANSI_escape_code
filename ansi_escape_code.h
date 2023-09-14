20dd1cf2e86bef85f2b77188a53aaedb938ed5b4
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-09-14 23:11:35 +0900

    add back ground

--- ansi_escape_code.h
+++ ansi_escape_code.h
@@ -24,4 +24,27 @@
 
 #define F_RESTET				"\033[39m"
 
+/*******************************************/
+/*            back ground color            */
+/*******************************************/
+
+#define B_BLACK					"\033[40m"
+#define B_RED					"\033[41m"
+#define B_GREEN					"\033[42m"
+#define B_YELLOW				"\033[43m"
+#define B_BLUE					"\033[44m"
+#define B_MAGENTA				"\033[45m"
+#define B_CYAN					"\033[46m"
+#define B_WHITE					"\033[47m"
+#define B_BRIGHT_BLACK			"\033[100m"
+#define B_BRIGHT_RED			"\033[101m"
+#define B_BRIGHT_GREEN			"\033[102m"
+#define B_BRIGHT_YELLOW			"\033[103m"
+#define B_BRIGHT_BLUE			"\033[104m"
+#define B_BRIGHT_MAGENTA		"\033[105m"
+#define B_BRIGHT_CYAN			"\033[106m"
+#define B_BRIGHT_WHIT			"\033[107m"
+
+#define B_RESTET				"\033[49m"
+
 #endif
